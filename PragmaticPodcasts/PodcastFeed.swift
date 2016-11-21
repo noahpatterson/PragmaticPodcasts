561e0aa87cc97171addf87b9d76044c37e14c82e
noahpatterson <n@noahpatterson.me>
2016-11-21 07:15:24 -0700

    Add Podcast Feed struct

---
+++ PragmaticPodcasts/PodcastFeed.swift
@@ -0,0 +1,19 @@
+//
+//  PodcastFeed.swift
+//  PragmaticPodcasts
+//
+//  Created by Noah Patterson on 11/19/16.
+//  Copyright © 2016 noahpatterson. All rights reserved.
+//
+
+import UIKit
+//UIImage needed for itunesImageUrl is in UIKit
+
+struct PodcastFeed {
+    var title:          String?
+    var link:           URL?
+    var description:    String?
+    var itunesAuthor:   String?
+    var itunesImageUrl: URL?
+}
+
