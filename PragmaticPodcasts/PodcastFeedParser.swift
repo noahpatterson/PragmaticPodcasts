d9afbe492e3ac2015939ea456a6c27cfba675346
noahpatterson <n@noahpatterson.me>
2016-11-25 18:31:46 -0700

    Fill out podcastFeed model

--- PragmaticPodcasts/PodcastFeedParser.swift
+++ PragmaticPodcasts/PodcastFeedParser.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 class PodcastFeedParser: NSObject, XMLParserDelegate {
+    var currentFeed: PodcastFeed?
+    var currentElementText: String?
+    
     init(contentsOf url: URL) {
         super.init()
     
@@ -27,8 +30,49 @@ class PodcastFeedParser: NSObject, XMLParserDelegate {
         dataTask.resume()
     }
     
+    // MARK: - XMLParserDelegate implementation​
     func parserDidStartDocument(_ parser: XMLParser) {
         NSLog("parserDidStartDocument, currently on line: \(parser.lineNumber)")
+        currentFeed = PodcastFeed()
     }
     
+    //callback that knows when we find a element we are looking for
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        switch elementName {
+            case "title", "link", "description", "itunes:image", "itunes:author":
+                currentElementText = ""
+            case "item":
+                parser.abortParsing()
+                NSLog("aborted parsing. podcastFeed = \(currentFeed)")
+            default:
+                currentElementText = nil
+            }
+    }
+    
+    //when the parser finds characters in the element. Is called multiple times so it's best to build the string.
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        currentElementText?.append(string)
+    }
+    
+    //when the parser reaches the end of an element, write the string to the model.
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        switch elementName {
+        case "title":
+            currentFeed?.title = currentElementText
+        case "link":
+            if let linkText = currentElementText {
+                currentFeed?.link = URL(string: linkText)
+            }
+        case "descrtiption":
+                currentFeed?.description = currentElementText
+        case "itunes:author":
+            currentFeed?.itunesAuthor = currentElementText
+        case "itunes:image":
+            if let urlText = currentElementText {
+                currentFeed?.itunesImageUrl = URL(string: urlText)
+            }
+        default:
+            break
+        }
+    }
 }
