a18896a13a783c51f09fcee206aab2a1cd8ccc66
noahpatterson <n@noahpatterson.me>
2016-11-29 14:41:08 -0700

    add and AddPodcast scene, viewController modal present segue, content in the view and a outlet for the urlField

---
+++ PragmaticPodcasts/AddPodcastViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddPodcastViewController.swift
+//  PragmaticPodcasts
+//
+//  Created by Noah Patterson on 11/29/16.
+//  Copyright © 2016 noahpatterson. All rights reserved.
+//
+
+import UIKit
+
+class AddPodcastViewController: UIViewController {
+
+    @IBOutlet var urlField: UITextField!
+    
+}
